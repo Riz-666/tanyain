97f8d85d2f1409056f4007d81042a6876a8c04f7
Riz-666 <rizthepublic666@gmail.com>
2025-08-01 14:12:14 +0700

    membuat web menjadi resposive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,6 +15,7 @@
             </a>
         @endif
     </header>
+
     <main class="frame">
         <section class="listbox-choose-blog">
             <div class="overlap-group">
@@ -28,35 +29,50 @@
                 </div>
             </div>
         </section>
-        <article class="background-border">
-            <h2 class="link-a-better">Cara Handle Laravel Error 500</h2>
-            <p class="disini-kita-akan">Disini kita akan<br />jelaskan error 500<br />dan cara handle nya</p>
-            <div class="attachment"> <img class="vector" src="img/vector.svg" alt="PDF icon" /> <span
-                    class="text-wrapper-2">Laravel_Error_500.pdf</span> </div>
-            <footer class="background">
-                <p class="p">dibuat Jul 20, 2010 at 22:27</p>
-                <div class="author-info"> <img class="topchef-s-user" src="img/image.png" alt="Mas Agung's avatar" />
-                    <div class="author-details"> <span class="text-wrapper-3">Mas Agung</span> <span
-                            class="text-wrapper-4">15.8k</span> </div>
-                </div>
-            </footer>
-        </article>
-        <article class="background-border">
-            <h2 class="link-a-better">Cara Handle Laravel Error 500</h2>
-            <div class="attachment"> <img class="vector" src="img/vector.svg" alt="PDF icon" /> <span
-                    class="text-wrapper-2">Laravel_Error_500.pdf</span> </div>
-            <p class="disini-kita-akan">Disini kita akan<br />jelaskan error 500<br />dan cara handle nya</p>
-            <footer class="background">
-                <p class="p">dibuat Jul 20, 2010 at 22:27</p>
-                <div class="author-info">
-                    <img class="topchef-s-user-2"
-                        src="https://tse3.mm.bing.net/th/id/OIP.4JF4xKSznSNzZHh1wEW_9gHaE8?r=0&rs=1&pid=ImgDetMain&o=7&rm=3"
-                        alt="Mas Agung's avatar" />
-                    <div class="author-details">
-                        <span class="text-wrapper-3">Mas Agung</span>
-                        <span class="text-wrapper-4">15.8k</span>
+
+        @foreach ($artikel as $art)
+            <a href="{{ route('article.detail', $art->id) }}" style="width: 100%">
+                <article class="background-border">
+                    <h2 class="link-a-better">{{ $art->judul }}</h2>
+                    <div class="isi"
+                        style="
+                    width: 1022px;
+                    height: 50px;
+                    margin-top:50px;
+                    color: #37474f;
+                    font-size: 20px;
+                    letter-spacing: 0.25px;
+                    line-height: 21px;
+                    width: 900px;
+                    white-space: nowrap;
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                    ">
+                        <small>{!! strip_tags($art->isi) !!}</small>
                     </div>
-                </div>
-            </footer>
-        </article>
-    @endsection
+                    <div class="attachment">
+                        <i class="fa-regular fa-file fa-2x vector"></i>
+                        @if ($art->repositori)
+                            <span class="text-wrapper-2">{{ $art->repositori->first()->nama_file }}</span>
+                        @else
+                            <span class="text-wrapper-2">Tidak ada File</span>
+                        @endif
+                    </div>
+
+                    <footer class="background">
+                        <p class="p">dibuat {{ $art->created_at }}</p>
+                        <div class="author-info">
+                            <img class="topchef-s-user"
+                                src="{{ asset($art->user->foto ? 'storage/user-img/' . $art->user_id : 'storage/user-img/default-user.jpg') }}"
+                                alt="{{ $art->user->nama }}" />
+                            <div class="author-details">
+                                <span class="text-wrapper-3">{{ $art->user->nama }}</span>
+                                <span class="text-wrapper-4 ">{{ $art->status }}</span>
+                            </div>
+                        </div>
+                    </footer>
+                </article>
+            </a>
+        @endforeach
+    </main>
+@endsection
